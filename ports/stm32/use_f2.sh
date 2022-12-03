96b879b4c4e1922e7999990f3b3fcfe5d872e655
Damien George <damien@micropython.org>
2022-12-03 17:49:01 +1100

    stm32: Add use_f2.sh helper script.
    Signed-off-by: Damien George <damien@micropython.org>

---
+++ ports/stm32/use_f2.sh
@@ -0,0 +1,5 @@
+# make this point to a local checkout of https://github.com/STMicroelectronics/STM32CubeF2.git
+cubef2=$HOME/proj/mp/github/STMicroelectronics/STM32CubeF2
+
+export STM32LIB_CMSIS_DIR=$cubef2/Drivers/CMSIS/Device/ST/STM32F2xx
+export STM32LIB_HAL_DIR=$cubef2/Drivers/STM32F2xx_HAL_Driver
