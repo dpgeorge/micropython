7e92b733eca217a630943de00ef14fd421a8b693
Damien George <damien@micropython.org>
2023-06-12 14:17:52 +1000

    webassembly: Expand override test.
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/webassembly/tests/basics/override_new.mjs
+++ ports/webassembly/tests/basics/override_new.mjs
@@ -3,16 +3,33 @@
 import {loadMicroPython} from '../../build/micropython.mjs';
 const runtime = await loadMicroPython({url: '../../build/micropython.wasm'});
 
-globalThis.MyClass = class {
+globalThis.MyClass1 = class {
     new() {
-        console.log("MyClass.new");
+        console.log("MyClass1 new");
+        return 1;
     }
 };
 
-globalThis.myClassInstance = new globalThis.MyClass();
+globalThis.MyClass2 = class {
+    static new() {
+        console.log("MyClass2 static new");
+        return 2;
+
+    }
+    new() {
+        console.log("MyClass2 new");
+        return 3;
+    }
+};
+
+globalThis.myClass2Instance = new globalThis.MyClass2();
 
 runtime.runPython(`
     import js
-    js.MyClass.new().new()
-    js.myClassInstance.new()
+
+    print(type(js.MyClass1.new()))
+    print(js.MyClass1.new().new())
+
+    print(js.MyClass2.new())
+    print(js.myClass2Instance.new())
 `);
