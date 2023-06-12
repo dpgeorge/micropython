6463d315ee337e4b6d04fedd548f934669f40337
Damien George <damien@micropython.org>
2023-06-12 14:03:23 +1000

    webassembly: Remove pre code for node.
    Doesn't seem to be needed anymore.
    
    Signed-off-by: Damien George <damien@micropython.org>

--- ports/webassembly/pyscript/extra.js
+++ ports/webassembly/pyscript/extra.js
@@ -1,6 +1,8 @@
+/*
 import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 var __dirname = "";
+*/
 
 export async function loadMicroPython(options) {
     const {heapsize, url, stdin, stdout, stderr} = Object.assign({heapsize: 1024 * 1024}, options);
